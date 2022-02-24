f7fffead56d2bf4773f4ecd01382e9d0f16d8e93
Devops-T27 <89909793+Devops-T27@users.noreply.github.com>
2022-02-24 02:58:40 -0500

    test

--- lab01.py
+++ lab01.py
@@ -2,22 +2,22 @@ student=[{
         "name":"alex",
         "age":20,
         "code":"AB02",
-        "notes":{12,5,8,20,14.5}},
+        "notes":(12,5,8,20,14.5)},
         {
         "name":"mourad",
         "age":42,
         "code":"AB01",
-        "notes":{10.25,5.5,8.3,18,10.5}},
+        "notes":(10.25,5.5,8.3,18,10.5)},
         {
         "name":"gaby",
         "age":260,
         "code":"AB03",
-         "notes":{11,0,16.25,19,14,5}},
+         "notes":(11,0,16.25,19,14,5)},
         {
         "name":"omar",
         "age":31,
         "code":"AB04",
-        "notes":{17,15,18,20,14,15}}
+        "notes":(17,15,18,20,14,15)}
         ]
 
 def studentAverge(student):
